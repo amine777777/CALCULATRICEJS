60e87fee995e801d87c94dbba6b95c55d482d793
amine777777 <90194084+amine777777@users.noreply.github.com>
2023-03-13 19:10:23 +0100

    kjuygfghi

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const touches = [...document.querySelectorAll('.bouton')];
 const listeKeycode = touches.map(touche => touche.dataset.key);
 const ecran = document.querySelector('.ecran');
 
-document.addEventListener('keydown', (e) => {
-    const valeur = e.keyCode.toString(); 
-    calculer(valeur)
+// document.addEventListener('keydown', (e) => {
+//     const valeur = e.keyCode.toString(); 
+//     calculer(valeur)
 
-})
+// })
 
 document.addEventListener('click', (e) => {
     const valeur = e.target.dataset.key;
